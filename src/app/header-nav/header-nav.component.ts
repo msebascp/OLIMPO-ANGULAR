2c58cb13a278e86234c8b5802682da6d52f8b84d
sebas <manuel_sebastiancriollo@elcampico.org>
2023-01-23 13:13:14 +0100

    añadido componente header_nav

---
+++ src/app/header-nav/header-nav.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-nav',
+  templateUrl: './header-nav.component.html',
+  styleUrls: ['./header-nav.component.scss']
+})
+export class HeaderNavComponent {
+
+}
