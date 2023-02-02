8c641ad6960d78c995b41ed1018da9225b07b563
Gabriel Sanchez <gabrielsanchezgarregs4@gmail.com>
2023-02-02 19:01:49 +0100

    inicio del home

--- src/app/header-nav/header-nav.component.ts
+++ src/app/header-nav/header-nav.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-header-nav',
+  selector: 'header-nav',
   templateUrl: './header-nav.component.html',
   styleUrls: ['./header-nav.component.scss']
 })
