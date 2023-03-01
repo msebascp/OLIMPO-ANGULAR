ced935306f5877f74f90f4bc9837bc63e9c4eb05
Alberto Zeron <albertozeronf@gmail.com>
2023-03-01 16:52:59 +0100

    modificacion del componente admin about y del media service para hacer llamadas a la api

--- src/app/admin/admin-about/admin-about.component.ts
+++ src/app/admin/admin-about/admin-about.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
-import {AuthPassportService} from "../../database/auth-passport.service";
-import {Router} from "@angular/router";
+import { AuthPassportService } from "../../database/auth-passport.service";
+import { Router } from "@angular/router";
 import { MediaService } from '../../database/media.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Information } from 'src/app/interfaces/information';
@@ -12,6 +12,74 @@ import Swal from 'sweetalert2';
   styleUrls: ['./admin-about.component.scss']
 })
 export class AdminAboutComponent {
+  isLogin: boolean = false;
+  aboutForm!: FormGroup
+  showInvalidSubmit: boolean = false;
+  media: Information = { id: 0, instagram: '', facebook: '', horario1: '', horario2: '' };
+  updatedMedia: Information = { id: 0, instagram: '', facebook: '', horario1: '', horario2: '' }
 
+  constructor(
+    private auth: AuthPassportService,
+    private router: Router,
+    private mediaService: MediaService,
+    private formBuilder: FormBuilder
+  ) { }
+
+  ngOnInit() {
+    this.auth.getVariable().subscribe(infoAuth => {
+      this.isLogin = infoAuth.isLogin
+    })
+    this.getAllAboutUs();
+    this.aboutForm = this.formBuilder.group(
+      {
+        instagram: ["", [Validators.required]],
+        facebook: ["", [Validators.required]],
+        horario1: ["", [Validators.required]],
+        horario2: ["", [Validators.required]]
+      }
+    )
+  }
+
+  getAllAboutUs() {
+    this.mediaService.getMedia().subscribe(information => {
+      information.forEach(media => {
+        this.media.id = media.id;
+        this.media.instagram = media.instagram;
+        this.media.facebook = media.facebook;
+        this.media.horario1 = media.horario1;
+        this.media.horario2 = media.horario2;
+      })
+    });
+  }
+
+  onSubmit() {
+    if (this.aboutForm.invalid) {
+      this.showInvalidSubmit = true
+      return;
+    }
+    const instagram = this.aboutForm.get('instagram')?.value || '';
+    const facebook = this.aboutForm.get('facebook')?.value || '';
+    const horario1 = this.aboutForm.get('horario1')?.value || '';
+    const horario2 = this.aboutForm.get('horario2')?.value || '';
+
+    this.updatedMedia.id = this.media.id;
+    this.updatedMedia.instagram = instagram;
+    this.updatedMedia.facebook = facebook;
+    this.updatedMedia.horario1 = horario1;
+    this.updatedMedia.horario2 = horario2;
+
+
+    this.mediaService.editMedia(this.media.id, this.updatedMedia).subscribe(_ => {
+      Swal.fire({
+        title: "<h5 style='color:white'>" + 'Información modificada' + "</h5>",
+        icon: 'success',
+        background: '#1F2937'
+      })
+    })
+  }
+
+  get form() {
+    return this.aboutForm.controls;
+  }
 
 }
--- src/app/database/media.service.ts
+++ src/app/database/media.service.ts
@@ -11,6 +11,30 @@ export class MediaService {
   constructor(
     private http: HttpClient
   ) { }
+  private API_URL = 'http://localhost:8000/api';
 
+  getMedia(): Observable<Information[]> {
+    return this.http.get<DataInformation>(this.API_URL + '/information').pipe(
+      map((data: DataInformation) => {
+        return data.data;
+      }),
+      catchError(e => {
+        console.error(e);
+        return [];
+      }),
+    );
+  }
+
+  editMedia(id: number, media: Information): Observable<Information> {
+    return this.http.post<Information>(this.API_URL + `/information/${id}?instagram=${media.instagram}&facebook=${media.facebook}&horario1=${media.horario1}&horario2=${media.horario2}`, media).pipe(
+      map((data: Information) => {
+        return data;
+      }),
+      catchError(e => {
+        console.error(e);
+        return [];
+      }),
+    )
+  }
 
 }
