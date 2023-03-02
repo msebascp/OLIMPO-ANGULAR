a9e8cf3f6114ce503df96b0baadaa88e339f55f9
Alberto Zeron <albertozeronf@gmail.com>
2023-03-02 18:33:05 +0100

    pequeños cambios

--- src/app/general/register-general/register-general.component.ts
+++ src/app/general/register-general/register-general.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import {Location} from "@angular/common";
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-register-general',
   templateUrl: './register-general.component.html',
   styleUrls: ['./register-general.component.scss']
 })
-export class RegisterGeneralComponent {
 
+
+export class RegisterGeneralComponent {
+  constructor (
+    private route: ActivatedRoute,
+    private location: Location,
+  ) { }
+  
+  public goBack(): void {
+    this.location.back();
+  }
 }
