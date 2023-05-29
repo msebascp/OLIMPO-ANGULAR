df47a6d503d2032d25498d2950223beebb19e868
sebas <manuel_sebastiancriollo@elcampico.org>
2023-05-29 19:11:17 +0200

    Terminando interceptor

--- src/app/database/media.service.ts
+++ src/app/database/media.service.ts
@@ -1,8 +1,8 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { BehaviorSubject, catchError, map, Observable } from 'rxjs';
-import { Information } from '../interfaces/information';
-import { DataInformation } from '../interfaces/dataInformation';
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {map, Observable} from 'rxjs';
+import {Information} from '../interfaces/information';
+import {DataInformation} from '../interfaces/dataInformation';
 
 @Injectable({
   providedIn: 'root'
@@ -10,18 +10,16 @@ import { DataInformation } from '../interfaces/dataInformation';
 export class MediaService {
   constructor(
     private http: HttpClient
-  ) { }
+  ) {
+  }
+
   private API_URL = 'http://localhost:8000/api';
 
   getMedia(): Observable<Information[]> {
     return this.http.get<DataInformation>(this.API_URL + '/information').pipe(
       map((data: DataInformation) => {
         return data.data;
-      }),
-      catchError(e => {
-        console.error(e);
-        return [];
-      }),
+      })
     );
   }
 
@@ -29,11 +27,7 @@ export class MediaService {
     return this.http.post<Information>(this.API_URL + `/information/${id}?instagram=${media.instagram}&facebook=${media.facebook}&horario1=${media.horario1}&horario2=${media.horario2}`, media).pipe(
       map((data: Information) => {
         return data;
-      }),
-      catchError(e => {
-        console.error(e);
-        return [];
-      }),
+      })
     )
   }
 
